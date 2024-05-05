71336e8810e2e1830625e0604e172de62a0c9a24
therealmahmoud~ <moha91875460@gmail.com>
2024-05-05 06:30:55 +0300

    updated

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,28 @@
+$("document").ready(function () {
+  let selected = {};
+  $('div.amenities input[type="checkbox"]').change(function () {
+    if ($(this).is(":checked")) {
+      selected[$(this).data("id")] = $(this).data("name");
+    } else {
+      delete selected[$(this).data("id")];
+    }
+    $("div.amenities h4").html(function () {
+      let amenities = [];
+      Object.keys(selected).forEach(function (key) {
+        amenities.push(selected[key]);
+      });
+      if (amenities.length === 0) {
+        return "&nbsp";
+      }
+      return amenities.join(", ");
+    });
+  });
+
+  $.ajax('http://0.0.0.0:5001/api/v1/status/').done(function (data) {
+    if (data.status === "OK") {
+      $("div#api_status").addClass("available");
+    } else {
+      $("div#api_status").removeClass("available");
+    }
+  });
+});
