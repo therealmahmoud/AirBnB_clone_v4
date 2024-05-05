300af415c84378551bc816a09ae9299e4cf25378
therealmahmoud~ <moha91875460@gmail.com>
2024-05-05 04:30:25 +0300

    updated

--- web_dynamic/1-hbnb.py
+++ web_dynamic/1-hbnb.py
@@ -8,6 +8,7 @@ from models.place import Place
 from os import environ
 from flask import Flask, render_template
 import uuid
+import jinja2
 app = Flask(__name__)
 # app.jinja_env.trim_blocks = True
 # app.jinja_env.lstrip_blocks = True
@@ -35,7 +36,7 @@ def hbnb():
     places = storage.all(Place).values()
     places = sorted(places, key=lambda k: k.name)
 
-    return render_template('0-hbnb.html',
+    return render_template('1-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
                            places=places,
